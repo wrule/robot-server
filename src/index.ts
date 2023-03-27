d271acd3f2a82a52b774df168835d766ea4b1b0c
jimao <jimao@jimaodeMacBook-Pro.local>
2023-03-27 17:30:26 +0800

    基本完成

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import koa from 'koa';
+import robot from 'robotjs';
+
+function main() {
+  const app = new koa();
+  app.use((ctx) => {
+    try {
+      const method = ctx.path.replace(/\//g, '');
+      if (method in robot) {
+        const args: any[] = Object.values(ctx.query)
+          .map((arg) => isNaN(Number(arg)) ? arg : Number(arg));
+        console.log(`robot.${method}(${args})`);
+        ctx.body = {
+          success: true,
+          data: (robot as any)[method](...args) || undefined,
+        };
+      } else {
+        ctx.body = {
+          success: false,
+        };
+      }
+    } catch (e) {
+      console.log(e);
+      ctx.body = {
+        success: false,
+      };
+    }
+  });
+  app.listen(7195);
+}
+
+main();
